6d53d2b253c03c8d731483dd671d2388fe43ddf8
Dan Morgan <ltdan681@gmail.com>
2020-07-11 14:37:28 -0400

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App(props) {
+
+  const [active, setActive] = useState(false)
+
+  useEffect(() => {
+    setActive(props.active)
+    if (active) {
+      const crosses = document.querySelectorAll(".cross")
+      crosses.forEach(cross => {
+        cross.style.width = "100%"
+      })
+      setTimeout(() => {
+        const left = document.querySelector("#left")
+        const right = document.querySelector("#right")
+        left.classList.add("active-left")
+        right.classList.add("active-right")
+      }, 400)
+    }
+  })
+
+
+  return (
+    <div id="test">
+      <div className="container">
+        <div
+          className="cross "
+          id="left"></div>
+          <div
+            className="cross "
+            id="right"></div>
+          </div>
+    </div>
+  )
+}
+
+function Test() {
+
+  const [state, setState] = useState(false)
+
+  const handleClick = () => {
+    setState(true)
+  }
+  return (
+    <div>
+      <App active={state} />
+      <button onClick={handleClick}>test</button>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Test />,
+  document.getElementById("root")
+)
